38260cebb10f7bf5af10c2faec89f929c8b2547c
tang <119taeyoung@naver.com>
2021-07-28 17:15:36 +0900

    뱅코 > 연혁

--- src/components/company/Organization.js
+++ src/components/company/Organization.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 152px - 101px);
+  overflow-y: scroll;
+`;
 
 const Organization = () => {
   return(
